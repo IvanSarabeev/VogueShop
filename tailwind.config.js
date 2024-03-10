de7626910f4727f449b45df4fb0bd3cf48c7082c
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-03-10 11:32:46 +0200

    feat: hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { "inter": "Inter, sans-serif" },
+      backgroundImage: {
+        'hero': "url('../../public/images/hero.jpg')",
+        'main-banner': "url('../../public/images/main-banner-1.jpg')",
+        'banner-two': "url('../../public/images/catbanner-01.jpg')",
+        'banner-three': "url('../../public/images/catbanner-03.jpg')",
+        'banner-four': "url('../../public/images/catbanner-02.jpg')",
+        'banner-five': "url('../../public/images/catbanner-04.jpg')",
+      },
       backgroundColor: {
-        'main': '#252525',
+        'main': '#fffffd',
         'header': '#c09578',
       },
       textColor: {
